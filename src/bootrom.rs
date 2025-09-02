51b8ecf0f3949dabc6b2a37f28381b8e5a31a48b
toms74209200 <motomotomato@gmail.com>
2025-09-02 22:07:17 +0900

    :sparkles: Update boot ROM

--- src/bootrom.rs
+++ src/bootrom.rs
@@ -3,7 +3,13 @@ pub struct Bootrom {
 }
 impl Bootrom {
     pub fn new(rom: Box<u8>) -> Self {
-        Self { rom }
+        Self { rom, active: true }
+    }
+    pub fn is_active(&self) -> bool {
+        self.active
+    }
+    pub fn write(&mut self, _: u16, val: u8) {
+        self.active &= val == 0;
     }
     pub fn read(&self, addr: u16) -> u8 {
         self.rom[addr as usize]
