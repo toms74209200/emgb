7c417e3fe689c5e38027881b1c2d0de8809bc864
toms74209200 <motomotomato@gmail.com>
2025-09-03 21:42:16 +0900

    :bug: Fix boot ROM

--- src/bootrom.rs
+++ src/bootrom.rs
@@ -1,8 +1,9 @@
 pub struct Bootrom {
-    rom: Box<u8>,
+    rom: Box<[u8]>,
+    active: bool,
 }
 impl Bootrom {
-    pub fn new(rom: Box<u8>) -> Self {
+    pub fn new(rom: Box<[u8]>) -> Self {
         Self { rom, active: true }
     }
     pub fn is_active(&self) -> bool {
