27cf5d7be1d9ccab27aec6582e6b35b97392f930
toms74209200 <motomotomato@gmail.com>
2025-11-20 22:57:04 +0900

    :sparkles: Add decode JR c NC

--- src/decode.rs
+++ src/decode.rs
@@ -7,6 +7,7 @@ impl cpu::Cpu {
         match self.ctx.opcode {
             0x00 => self.nop(bus),
             0x20 => self.jr_c(bus, operand::Cond::NZ),
+            0x30 => self.jr_c(bus, operand::Cond::NC),
             _ => unimplemented!("opcode {:02x} not implemented", self.ctx.opcode),
         }
     }
@@ -40,6 +41,24 @@ mod tests {
         let mut peripherals = peripherals::Peripherals::new(bootrom);
         cpu.regs.pc = 0xc000;
         peripherals.write(0xc000, 0x20);
+        peripherals.write(0xc001, 0x05);
+        cpu.fetch(&peripherals);
+        let initial_pc = cpu.regs.pc;
+        cpu.decode(&mut peripherals);
+        cpu.decode(&mut peripherals);
+        cpu.decode(&mut peripherals);
+        assert_eq!(cpu.regs.pc, initial_pc + 7);
+    }
+    #[test]
+    fn test_decode_jr_c_nc() {
+        let mut cpu = cpu::Cpu {
+            regs: crate::registers::Registers::default(),
+            ctx: cpu::Ctx::default(),
+        };
+        let bootrom = crate::bootrom::Bootrom::new(vec![0; 256].into_boxed_slice());
+        let mut peripherals = peripherals::Peripherals::new(bootrom);
+        cpu.regs.pc = 0xc000;
+        peripherals.write(0xc000, 0x30);
         peripherals.write(0xc001, 0x05);
         cpu.fetch(&peripherals);
         let initial_pc = cpu.regs.pc;
